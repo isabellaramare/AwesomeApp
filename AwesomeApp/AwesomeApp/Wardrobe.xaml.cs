c36833ecfa87bdd3c6a2ec6fc4377a19c927cf98
isabellaramare <isabella.ramare@hotmail.com>
2020-10-27 11:35:31 +0100

    Add project files.

---
+++ AwesomeApp/AwesomeApp/Wardrobe.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomeApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Wardrobe : ContentPage
+    {
+        public Wardrobe()
+        {
+            InitializeComponent();
+        }
+
+        private void GoBack_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
